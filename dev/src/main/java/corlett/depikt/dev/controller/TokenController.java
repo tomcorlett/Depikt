789f9fbc3195b68ccd437f9c91ee3f8113a81ce5
tomcorlett <tomcorlettwsfc@hotmail.com>
2021-08-11 15:15:15 +0100

    refactored token api into it's own class

---
+++ dev/src/main/java/corlett/depikt/dev/controller/TokenController.java
@@ -0,0 +1,89 @@
+package corlett.depikt.dev.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import corlett.depikt.dev.model.Member;
+import corlett.depikt.dev.model.Role;
+import corlett.depikt.dev.model.RoleToMemberForm;
+import corlett.depikt.dev.service.MemberServiceImpl;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.JWTVerifier;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpStatus.FORBIDDEN;
+
+@RestController
+@RequestMapping(path = "api/v1/token")
+public class TokenController {
+    private final MemberServiceImpl memberService;
+
+    @Autowired
+    public TokenController(MemberServiceImpl memberService) {
+        this.memberService = memberService; 
+    }
+
+    //Token api 
+    @GetMapping("/refresh")
+    public void refreshToken(HttpServletRequest request, HttpServletResponse response) throws JsonGenerationException, JsonMappingException, IOException {     
+        String authorizationHeader = request.getHeader(AUTHORIZATION);
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            try {
+                String refreshToken = authorizationHeader.substring("Bearer ".length());
+                //TODO: refactor into utils class v
+                Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
+                JWTVerifier verifier =  JWT.require(algorithm).build();
+                DecodedJWT decodedJWT = verifier.verify(refreshToken);
+                String username = decodedJWT.getSubject();
+                Member member = memberService.findByEmail(username);
+                String accessToken = JWT.create()
+                    .withSubject(member.getEmail())
+                    .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000))
+                    .withIssuer(request.getRequestURL().toString())
+                    .withClaim("roles", member.getRoles().stream().map(Role::getName).collect(Collectors.toList()))
+                    .sign(algorithm);
+                Map<String, String> tokens = new HashMap<>();
+                tokens.put("access_token", accessToken);
+                tokens.put("refresh_token", refreshToken);
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), tokens);                
+            } catch (Exception e) {
+                
+                response.setHeader("error", e.getMessage());
+                response.setStatus(FORBIDDEN.value());
+                Map<String, String> error = new HashMap<>();
+                error.put("error", e.getMessage());
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+                new ObjectMapper().writeValue(response.getOutputStream(), error);
+
+            }
+        } else {                
+            throw new RuntimeException("refresh token is missing");
+        }          
+    }
+    
+}
